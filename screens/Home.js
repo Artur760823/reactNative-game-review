26162a9f4108e5ea40d48ab4dbccdc24fa750ebb
Artur <artur.zajaczkowski76@gmail.com>
2022-07-11 11:03:22 +0200

    styles

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import {useState} from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-const Home = () => {
+const Home = ({navigation}) => {
+
+  const [reviews, setReviews] = useState([
+    {title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1'},
+    {title: 'The Elders Scroll: Skyrim', rating: 7, body: 'lorem ipsum', key: '2'},
+    {title: 'Workers&Resources Soviet Republic', rating: 5, body: 'lorem ipsum', key: '3'},
+  ])
+
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home screen :-)</Text>
+      <FlatList
+        data={reviews}
+        renderItem = {({item})=>(
+          <TouchableOpacity onPress={()=> navigation.navigate('Review', item)}>
+            <Text style={globalStyles.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      
+      />
+
     </View>
   )
 }
--- screens/Review.js
+++ screens/Review.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 
-const Review = () => {
+const Review = ({navigation}) => {
+
   return (
     <View style={globalStyles.container}>
-      <Text>Review screen</Text>
+      <Text>Game Title: {navigation.getParam('title')}</Text>
+      <Text>Review: {navigation.getParam('body')}</Text>
+      <Text>Rating: {navigation.getParam('rating')}</Text>
     </View>
   )
 }
